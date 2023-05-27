6042696dadc8805385276e734ac70c979713939f
lciamp <l.ciamp@yahoo.com>
2023-05-27 13:57:26 -0400

    test

--- blockchain-info-app/src/blockchain_info.rs
+++ blockchain-info-app/src/blockchain_info.rs
@@ -2,4 +2,11 @@ use reqwest;
 use tokio;
 use serde::Result;
 
-const HOST_ROOT: &str = "https://btcbook.nownodes.io/api/"
+const HOST_ROOT: &str = "https://btcbook.nownodes.io/api/";
+
+pub fn send_request(url: &str) -> String {
+
+    let client = reqwest::Client::new();
+    client.get(url)
+
+}
