33651b083e674882e483991b91d7d6ed693eb1be
lciamp <l.ciamp@yahoo.com>
2023-05-27 12:05:23 -0400

    struct for backend

--- blockchain-info-app/src/blockchain_status.rs
+++ blockchain-info-app/src/blockchain_status.rs
@@ -1,6 +1,18 @@
-// struct fror marshaling json
+// struct fopr marshaling the json response
+struct fror marshaling json
 pub struct BlockchainStatus {
 	pub blockbook: Blockbook,
 	pub backend: Backend,
 }
 
+pub struct Backend {
+    pub chain: String,
+     blocks: i64,
+     headers: i64,
+     best_block_hash: String,
+     difficulty: String,
+     size_on_disk: i64,
+     version: String,
+     subversion: String,
+     protocol_version: String
+}
