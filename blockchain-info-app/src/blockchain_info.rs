3ce9017fb253ce0cf3efb5f926bdc669bc7154ff
lciamp <l.ciamp@yahoo.com>
2023-05-27 14:31:42 -0400

    fix

--- blockchain-info-app/src/blockchain_info.rs
+++ blockchain-info-app/src/blockchain_info.rs
@@ -1,3 +1,4 @@
+use dotrnv;
 use reqwest;
 use tokio;
 use serde::Result;
@@ -7,6 +8,8 @@ const HOST_ROOT: &str = "https://btcbook.nownodes.io/api/";
 pub fn send_request(url: &str) -> String {
 
     let client = reqwest::Client::new();
-    client.get(url)
+    client
+        .get(url)
+        .header("api-key", "01cc171a-383b-4584-ab5a-14159b5dc100")
 
 }
