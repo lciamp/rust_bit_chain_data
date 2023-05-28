1360686e6cf83b2a4cba09b7e37073e241a51973
lciamp <l.ciamp@yahoo.com>
2023-05-28 12:02:30 -0400

    add locals and serde to main

--- blockchain-info-app/src/main.rs
+++ blockchain-info-app/src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate serde_derive;
+
+mod blockchain_info;
+mod blockchain_address;
+mod blockchain_status;
+mod blockchain_transactions;
+
 fn main() {
     println!("Hello, world!");
 }
