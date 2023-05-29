ee64bfacd778700c3a4fdd06391cfc7e60a22cbd
lciamp <l.ciamp@yahoo.com>
2023-05-29 11:24:49 -0400

    add wallet

--- blockchain-info-app/src/main.rs
+++ blockchain-info-app/src/main.rs
@@ -14,11 +14,12 @@ fn blockchain_info_app(address: &str){
     let blockchain_status: BlockchainStatus = blockchain_info::blockchain_status_request();
     println!("\n\nQuerying {} - chain: {}\n\n", &blockchain_status.blockbook.coin, &blockchain_status.backend.chain);
     
-    let blockchain_address: BlockchainAddress = blockchain_info::blockchain_address_request(address;)
+    let blockchain_address: BlockchainAddress = blockchain_info::blockchain_address_request(address);
     println!("\nAnalyzing ttransactions for Bitcoin address: {}", &blockchain_address.address)
 }
 
 fn main() {
-
+    let entered_address = dotenv::var("WALLET").expect("Error reading env var.");
+    blockchain_info_app(&entered_address);
 
 }
